cf2835cbb6de67e72bea62151249b50b544cd11f
xEEnzo <qmtri@apcs.vn>
2016-09-03 18:50:15 +0700

    Commit on branch
    remove redundancy comments
    change names of some local variable

--- app/src/main/java/com/example/triquach/finalproj/OpenExistingActivity.java
+++ app/src/main/java/com/example/triquach/finalproj/OpenExistingActivity.java
@@ -18,29 +18,18 @@ public class OpenExistingActivity extends AppCompatActivity {
 
 
         mainListView = (ListView) findViewById( R.id.listView );
-
-        // Create and populate a List of planet names.
         String[] planets = new String[] { "Proj 1", "Proj 2", "Proj 3", "Proj 4",
                 "Proj 5", "Proj 6", "Proj 7", "Proj 8"};
         ArrayList<String> planetList = new ArrayList<String>();
         planetList.addAll( Arrays.asList(planets) );
 
-        // Create ArrayAdapter using the planet list.
         listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, planetList);
-
-        // Add more planets. If you passed a String[] instead of a List<String>
-        // into the ArrayAdapter constructor, you must not add more items.
-        // Otherwise an exception will occur.
         listAdapter.add( "Proj x" );
         listAdapter.add( "Proj x" );
         listAdapter.add( "Proj x" );
         listAdapter.add( "Proj x" );
         listAdapter.add( "Proj x" );
         listAdapter.add( "Proj x" );
-
-
-
-        // Set the ArrayAdapter as the ListView's adapter.
         mainListView.setAdapter( listAdapter );
     }
 }
