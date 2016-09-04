28bbde6a6d54702b896c8b9532632550e713346f
Linh VO <jennylinh95@gmail.com>
2016-09-04 22:29:04 +0700

    Lock screen orientation

--- app/src/main/java/com/example/triquach/finalproj/MainActivity.java
+++ app/src/main/java/com/example/triquach/finalproj/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.triquach.finalproj;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -29,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
     }
 }
