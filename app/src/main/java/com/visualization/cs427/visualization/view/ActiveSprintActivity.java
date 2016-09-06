f9b5282f9781e6ccf1cc645e47099674422c53e4
Toan Nguyen <nbtoan@apcs.vn>
2016-09-06 15:11:43 +0700

    aaa

---
+++ app/src/main/java/com/visualization/cs427/visualization/view/ActiveSprintActivity.java
@@ -0,0 +1,15 @@
+package com.visualization.cs427.visualization.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.visualization.cs427.visualization.R;
+
+public class ActiveSprintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_active_sprint);
+    }
+}
