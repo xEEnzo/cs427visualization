4687a5a12b65d9fc2fcd9f4497202427bdfcf07e
xEEnzo <qmtri@apcs.vn>
2016-09-08 15:20:29 +0700

    [Fixed] Based on comments
    Check this pls

--- app/src/main/java/com/visualization/cs427/visualization/view/CreateIssueActivity.java
+++ app/src/main/java/com/visualization/cs427/visualization/view/CreateIssueActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.visualization.cs427.visualization.R;
 
@@ -17,10 +20,16 @@ public class CreateIssueActivity extends AppCompatActivity {
     {
         setContentView(R.layout.activity_sprint_detail);
     }
+    private Spinner spinnerIssueType,spinnerAssignee,spinnerEpic,spinnerLinkedIssue,spinnerIssue;
+    private TextView TextViewAddEpic;
+    private Button Create,Cancle;
+    private ImageView ImageAddEpic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_issue2);
+
+        /*-------------------------------- set content for Spinner -------------------------------*/
 
         Spinner dropdownIssueType = (Spinner)findViewById(R.id.spinnerIssueType);
         String[] items = new String[]{"Story", "Bug", "Task"};
@@ -46,5 +55,25 @@ public class CreateIssueActivity extends AppCompatActivity {
         String[] itemsIssue = new String[]{"Issue1", "Issue2","Issue3","Issue4"};
         ArrayAdapter<String> adapterIssue = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, itemsIssue);
         dropdownIssue.setAdapter(adapterIssue);
+
+        /*-------------------------------- set content for Spinner -------------------------------*/
+
+
+
+        /*-------------------------------------findViewById---------------------------------------*/
+        spinnerIssueType = (Spinner) findViewById(R.id.spinnerIssueType);
+        spinnerAssignee = (Spinner) findViewById(R.id.spinnerAssignee);
+        spinnerEpic = (Spinner) findViewById(R.id.spinnerEpic);
+        spinnerLinkedIssue = (Spinner) findViewById(R.id.spinnerLinkedIssues);
+        spinnerIssue = (Spinner) findViewById(R.id.spinnerIssue);
+
+        ImageAddEpic = (ImageView) findViewById(R.id.ImageAddEpic);
+        Create = (Button) findViewById(R.id.Create);
+        Cancle = (Button) findViewById(R.id.Cancle);
+
+        TextViewAddEpic = (TextView) findViewById(R.id.TextViewAddEpic);
+
+        /*-------------------------------------findViewById---------------------------------------*/
+
     }
 }
